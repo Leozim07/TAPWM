d7b5d0f1ec8f454b11102f1cce30f968e1b60cb5
Leo <102178909+Leozim07@users.noreply.github.com>
2025-10-22 18:11:14 -0300

    Add files via upload

---
+++ Atividade-21/app/routes/professores.js
@@ -0,0 +1,34 @@
+//let dbConnection = require('../config/DbConnection');
+module.exports = function(app){
+   app.get('/informacao/professores', function(req,res){
+       const sql = require ('mssql');
+ 
+       const sqlConfig = {
+           user: 'BD2313018', //7 últimos dígitos do seu RA
+           password: 'Teste1234',
+           database: 'BD', 
+           server: 'APOLO',
+           options: {
+               encrypt: false,
+               trustServerCertificate: true,
+           }
+       }
+  
+// 
+      async function getProfessores() {
+          try {
+                let connection = app.config.dbConnection;
+                const pool = await connection();
+                const results = await pool.request().query('SELECT * from PROFESSORES')
+          
+              // res.json(results.recordset);
+ 
+              res.render('informacao/professores',{profs: results.recordset})
+    
+           } catch (err) {
+               console.log(err)
+          }
+       }
+      getProfessores();
+   });
+}
